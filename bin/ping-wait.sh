8846a7ebeacd157b1812379fe5c57989bfc85b29
Tim Fletcher <tim@night-shade.org.uk>
2018-03-11 12:23:21 +0100

    Rework ping-wait to be more portable
    Remove -w flag from ping (no supported on macos)
    Change from echo -n to printf

--- bin/ping-wait.sh
+++ bin/ping-wait.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-echo -n "Waiting: "
-while ! ping -q -c 2 -w 10 $1 > /dev/null ; do
-	sleep 10
-	echo -n "."
+printf "Waiting: "
+while ! ping -q -c 2 $1 1>&2> /dev/null ; do
+    sleep 10
+    printf "."
 done
-echo " $1 is alive!"
+echo " $1 is alive!" ; sleep 10
 shift
-$*
+exec $*
