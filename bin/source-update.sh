ecb428771bdc115679c78f94bb64860723e7b722
Tim Fletcher <timothy.fletcher@cubits.com>
2018-04-10 10:04:26 +0200

    Fix up script for macOS support with gfind

--- bin/source-update.sh
+++ bin/source-update.sh
@@ -1,25 +1,38 @@
 #!/bin/sh
 SOURCEDIR=$(greadlink -f ${1:-./})
 ORIGDIR=$(pwd)
+OS=$(uname -s)
+case $OS in
+    Darwin)
+        FINDCMD=gfind
+        ;;
+    Linux)
+        FINDCMD=find
+        ;;
+    *)
+        echo Unknown OS $OS, exiting
+        exit 1
+        ;;
+esac
 
-for dir in $(find $SOURCEDIR -mindepth 1 -maxdepth 3 -type d -name .svn ); do
+for dir in $($FINDCMD $SOURCEDIR -mindepth 1 -maxdepth 3 -type d -name .svn ); do
     echo $dir ; cd $dir/.. ; svn upgrade
     echo $dir ; cd $dir/.. ; svn up
 done
 
-for dir in $(find $SOURCEDIR -mindepth 1 -maxdepth 3 -type d -name CVS ); do
+for dir in $($FINDCMD $SOURCEDIR -mindepth 1 -maxdepth 3 -type d -name CVS ); do
     echo $dir ; cd $dir/.. ; cvs up
 done
 
-for dir in $(find $SOURCEDIR -mindepth 1 -maxdepth 3 -type d -name .hg ); do
-    echo $dir ; cd $dir ; hg up
+for dir in $($FINDCMD $SOURCEDIR -mindepth 1 -maxdepth 3 -type d -name .hg ); do
+    echo $dir ; cd $dir/.. ; hg up
 done
 
-for dir in $(find $SOURCEDIR -mindepth 1 -maxdepth 3 -type d -name .bzr ); do
-    echo $dir ; cd $dir ; bzr up
+for dir in $($FINDCMD $SOURCEDIR -mindepth 1 -maxdepth 3 -type d -name .bzr ); do
+    echo $dir ; cd $dir/.. ; bzr up
 done
 
-for dir in $(find $SOURCEDIR -mindepth 1 -maxdepth 3 -type d -name .git ); do
+for dir in $($FINDCMD $SOURCEDIR -mindepth 1 -maxdepth 3 -type d -name .git ); do
     echo $dir ; cd $dir/..  ; git fetch
 done
 
