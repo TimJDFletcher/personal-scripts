d962edc6487645376c5df7cb8354e3a0875c3504
Tim Fletcher <tim@night-shade.org.uk>
2018-06-12 08:34:15 +0200

    Add caching, pass in variables

--- bin/random-play.sh
+++ bin/random-play.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-DIR=${1:-./}
-find $DIR -type f -print0 | xargs -0 touch
-find $DIR -type f -print0 | sort -R -z| xargs -0 mplayer -xy .75
+DIR=.
+CACHE=8192
+find $DIR -type f -print0 | sort -R -z| xargs -0 mplayer -cache $CACHE $*
