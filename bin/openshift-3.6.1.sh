0e823c7dbb3389a6d11bb00f425b21c77297c25f
Tim Fletcher <tfletcher@thoughtworks.com>
2018-07-16 11:28:39 +0200

    Add a script to install an older version of openshift

---
+++ bin/openshift-3.6.1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+PACKAGE=openshift-cli
+VERSION=3.6.1
+SHA=537d08de696bba4b5124e8818aba1e512fad49d6
+
+brew install ${PACKAGE}
+brew unlink ${PACKAGE}
+cd "$(brew --repo homebrew/core)"
+git checkout -b ${PACKAGE}-${VERSION} ${SHA}
+HOMEBREW_NO_AUTO_UPDATE=1 brew install --ignore-dependencies ${PACKAGE}
+git checkout master
+git branch -d ${PACKAGE}-${VERSION}
+brew list ${PACKAGE} --versions
+brew switch ${PACKAGE} ${VERSION}
