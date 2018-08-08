ecb23cad5beef059f1c67537e223305971c8ad6d
Tim Fletcher <tfletcher@thoughtworks.com>
2018-08-08 13:27:02 +0200

    Add a pin command to the script

--- bin/openshift-3.6.1.sh
+++ bin/openshift-3.6.1.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 PACKAGE=openshift-cli
 VERSION=3.6.1
 SHA=537d08de696bba4b5124e8818aba1e512fad49d6
@@ -8,6 +8,7 @@ brew unlink ${PACKAGE}
 cd "$(brew --repo homebrew/core)"
 git checkout -b ${PACKAGE}-${VERSION} ${SHA}
 HOMEBREW_NO_AUTO_UPDATE=1 brew install --ignore-dependencies ${PACKAGE}
+brew pin ${PACKAGE}
 git checkout master
 git branch -d ${PACKAGE}-${VERSION}
 brew list ${PACKAGE} --versions
