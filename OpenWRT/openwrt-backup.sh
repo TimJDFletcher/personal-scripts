cd62c0ca7f6f141afbb3402cdddab2a624a2f311
Tim Fletcher <tfletcher@thoughtworks.com>
2019-12-14 18:53:41 +0100

    Reformat for readablity

--- OpenWRT/openwrt-backup.sh
+++ OpenWRT/openwrt-backup.sh
@@ -6,5 +6,6 @@ if [ -z $host ] ; then
         echo "Usage $0 <hostname>"
         exit 1
 fi
-
-ssh $host sysupgrade -b - | gpg2 -e -r $recipient > $dir/$host-backup-$(date +%Y%m%d-%H%M).tar.gz.gpg
+mkdir -p $dir
+ssh $host sysupgrade -b - | \
+    gpg -e -r $recipient > $dir/$host-backup-$(date +%Y%m%d-%H%M).tar.gz.gpg
