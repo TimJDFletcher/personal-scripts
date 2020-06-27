588cb3f2b9dc99ddc6e5a0827f1e8ae382ebf2f4
Tim Fletcher <tim@night-shade.org.uk>
2020-06-27 11:36:13 +0200

    Make the backup script take a parm for the target

--- OpenWRT/openwrt-backup-raw.sh
+++ OpenWRT/openwrt-backup-raw.sh
@@ -1,5 +1,9 @@
-#!/bin/sh
-host=wt3020
+#!/bin/bash
+set -e -u
+host=${1}
 recipient=tim@night-shade.org.uk
 dir=$HOME/OpenWRT
-ssh $host tar -c -C /overlay/upper ./ | pigz -c | gpg2 -e -r $recipient > $dir/wt3020-backup-raw-$(date +%Y%m%d-%H%M).tar.gz.gpg
+
+ssh $host tar -c -C /overlay/upper ./ |
+  pigz -c |
+  gpg -e -r $recipient >"$dir/$host-backup-raw-$(date +%Y%m%d-%H%M)".tar.gz.gpg
