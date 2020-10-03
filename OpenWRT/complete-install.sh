627d4df587d2f72ec4289bf906f01061e7160794
Tim Fletcher <tim@night-shade.org.uk>
2020-10-03 10:44:45 +0200

    Remove openssl as OpenWRT includes wolfssl now

--- OpenWRT/complete-install.sh
+++ OpenWRT/complete-install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-ssl_support="ca-bundle ca-certificates libustream-openssl"
+ssl_support="ca-bundle ca-certificates"
 packages="avahi-daemon-service-ssh bmon etherwake tor sqm-scripts ipset ${ssl_support}"
 services="tor sqm"
 
