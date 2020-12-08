8ffd609044cef5ff61c31a8f5c49e7ea68900414
Tim Fletcher <tfletcher@thoughtworks.com>
2020-12-08 10:41:23 +0100

    Switch away from gpg tools

--- bin/restart-gpg-agent.sh
+++ bin/restart-gpg-agent.sh
@@ -5,5 +5,5 @@ pkill -9 scdaemon
 pkill -9 dirmngr
 pkill -9 gnupg-pkcs11-scd
 sleep 1
-#eval $($(brew --prefix gnupg)/bin/gpg-agent --daemon)
-eval $(/usr/local/MacGPG2/bin/gpg-agent --daemon)
+eval $($(brew --prefix gnupg)/bin/gpg-agent --daemon)
+#eval $(/usr/local/MacGPG2/bin/gpg-agent --daemon)
