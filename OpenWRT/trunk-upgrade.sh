8a67558510824fa0305e7af3e52964a8c6fe1678
Tim Fletcher <tim@night-shade.org.uk>
2021-03-27 17:09:00 +0100

    Reorder upgrade script, simplify remote unlock script

--- OpenWRT/trunk-upgrade.sh
+++ OpenWRT/trunk-upgrade.sh
@@ -5,14 +5,14 @@ arch=ipq806x/generic
 tempdir=$(mktemp -d /tmp/sysupgrade.XXXXXX)
 packages="coreutils-sha256sum"
 
-opkg update
-opkg install -d ram ${packages}
-
 cd $tempdir
 wget https://downloads.openwrt.org/snapshots/targets/$arch/$image
 wget https://downloads.openwrt.org/snapshots/targets/$arch/sha256sums
 
-/tmp/usr/bin/gnu-sha256sum --ignore-missing -c sha256sums
+opkg update
+opkg install -d ram ${packages}
+
+/tmp/usr/libexec/sha256sum-coreutils --ignore-missing -c sha256sums
 
 echo sha256 checksum confirmed, flashing in 10 seconds
 sleep 12
--- bin/unlock-oxygen.sh
+++ bin/unlock-oxygen.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -eu -o pipefail
 
-host=root@oxygen
+host=root@192.168.8.2
 
 _print_password ()
 {
-    gopass disks/oxygen/root
+    gopass show disks/oxygen/root
 }
 
 _ensure_gopass_latest ()
@@ -18,5 +18,4 @@ unlock_remote_luks ()
     _print_password | ssh $host cryptroot-unlock
 }
 
-_ensure_gopass_latest
 unlock_remote_luks
