5f202a2826da4692fbf2131dc79ea7c353d49040
Tim Fletcher <tim@night-shade.org.uk>
2022-03-07 10:04:49 +0000

    Update targets

--- bin/tv-sync.sh
+++ bin/tv-sync.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 set -eu -o pipefail
-HOST="tim@oxygen.vpn.night-shade.org.uk"
-TARGET_DIR="/media/transmission/downloads/complete"
+HOST="${1:-tim@oxygen.vpn.night-shade.org.uk}"
+TARGET_DIR="${2:-/WDZBA365/media/transmission/downloads/complete}"
 SRC_DIR=/media/tv
+SYNC_DAYS=30
 
 _rsync()
 {
-    echo "Copying TV from $PWD to ${HOST}:${TARGET_DIR}"
+    echo "Copying TV from ${1} to ${HOST}:${TARGET_DIR}"
         rsync \
             --recursive \
+            --size-only \
             --verbose \
             --progress \
-            --bwlimit 500K \
-            --filter="+ *.mkv" \
-            --filter="+ *.mp4" \
-            --filter="- *" \
+            --bwlimit 600K \
+            --temp-dir ../incomplete \
+            --from0 \
+            --files-from=<(find "${1}" -ctime -${SYNC_DAYS} -type f \( -iname \*.mp4 -o -iname \*.mkv \) -print0) \
             . ${HOST}:${TARGET_DIR}
+
 }
 
 cat $HOME/tv.list | while read line ; do 
-    cd "$SRC_DIR/$line/"
-    _rsync
+    if [ -d "$SRC_DIR/$line" ] ; then
+        cd "$SRC_DIR"
+        _rsync "${line}"
+    else
+        echo "Skipping $SRC_DIR/$line"
+    fi
 done
